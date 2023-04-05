ac9aa8062c86e3550ac5d42c30f249b072b7246d
heleena-fadi <miss.heleena98@gmail.com>
2023-04-05 14:05:48 +0300

    creat error pages 404 and 500 Relates#16 closes#16

--- server/router/router.js
+++ server/router/router.js
@@ -1,10 +1,21 @@
 const getNote = require('../controllers/getnote');
 const addNote = require('../controllers/addnote');
-
+const {join} = require('path');
 const router = require('express').Router();
 
 router.get('/notes', getNote);
 router.post('/create', addNote);
 
+router.use((req, res) => {
+    res
+      .status(404)
+      .sendFile(join(__dirname, '..', '..', 'public', 'html', '404.html'));
+  });
+
+  router.use((err ,req, res, next) => {
+    res
+      .status(500)
+      .sendFile(join(__dirname, '..', '..', 'public', 'html', '500.html'));
+  });
 
 module.exports = router;
