c6fe4cc4449cde16d59c8855dbc5ec85758be730
HamdyPs <redwanhmdyrdwan@gmail.com>
2023-04-05 15:28:13 +0300

    add test router end points Relate #22 closes #22

---
+++ server/__test__/test.js
@@ -0,0 +1,36 @@
+const { log } = require('console');
+const app = require('../app');
+const request = require('supertest');
+
+test('Get /notes status 200 json get all notes', (done) => {
+  request(app)
+    .get('/notes')
+    .expect(200)
+    .expect('Content-Type', /json/)
+    .end((err, res) => {
+      if (err) {
+        return done(err);
+      }
+      console.log(res.body.data);
+      expect(res.body.data.length).toBe(25);
+      return done();
+    });
+});
+
+test('POST /create status 201 json get created note', (done) => {
+  request(app)
+    .post('/create')
+    .send({
+      title: "asd",
+      content: "asd",
+    })
+    .expect(201)
+    .expect('Content-Type', /json/)
+    .end((err, res) => {
+      if (err) {
+        return done(err);
+      }
+      expect(res.body.data.rows.length).toBe(1);
+      return done();
+    });
+});
